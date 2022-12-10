38c219dfd54b83f48a9edbb1c440aaa97a24a06e
Nam1107 <teenhainam2603@gmail.com>
2022-12-11 03:20:35 +0700

    up

--- js/authAdmin.js
+++ js/authAdmin.js
@@ -1,15 +1,30 @@
+var token = localStorage.getItem('token');
+var refreshToken = localStorage.getItem('refreshToken');
+var statusAuth = '';
+var email = '';
 $.ajax({
-  url: ROOT + "/server/controllers/authen.php",
-  type: 'post',
-  async: false,
-  data: {
-    'authenLogin': 1,
+  url: ROOT + "user/getProfile",
+  type: 'GET',
+  headers: {
+    "Authorization": 'Bearer ' + token,
   },
-  success: function (data) {
-    var obj = JSON.parse(data);
-    if (obj.status != 'admin') {
-      window.open(ROOT + "/client/homepage/index.html", "_self");
-    }
-
+  async: false
+}).done(function (data) {
+  var obj = JSON.parse(data);
+  if (obj.obj == null) {
+    localStorage.clear();
+    window.open(PATH + "auth/login.html", "_self");
   }
+  if (obj.obj.role == "ROLE_ADMIN") {
+    statusAuth = obj.obj.role;
+    email = obj.obj.email;
+  } else if (obj.obj.role == "ROLE_SHIPPER") {
+    window.open(PATH + "cmsShipper/shippers/index.html", "_self");
+  } else {
+    localStorage.clear();
+  }
+
+}).fail(async function (data) {
+  var obj = JSON.parse(data['responseText']);
+  console.log(obj);
 });
--- js/authShipper.js
+++ js/authShipper.js
@@ -1,6 +1,8 @@
 var token = localStorage.getItem('token');
 var statusAuth = '';
 var email = '';
+var shipperID = '';
+var profile;
 $.ajax({
   url: ROOT + "user/getProfile",
   type: 'GET',
@@ -10,14 +12,22 @@ $.ajax({
   async: false
 }).done(function (data) {
   var obj = JSON.parse(data);
+
   if (obj.obj == null) {
     localStorage.clear();
     window.open(PATH + "auth/login.html", "_self");
   }
-  // if (obj.obj.role == "ROLE_USER") {
-  statusAuth = obj.obj.role;
-  email = obj.obj.email;
-  // }
+  profile = obj;
+  shipperID = obj.obj.ID;
+  if (obj.obj.role == "ROLE_SHIPPER") {
+    statusAuth = obj.obj.role;
+    email = obj.obj.email;
+  } else if (obj.obj.role == "ROLE_ADMIN") {
+    window.open(PATH + "cms/orders/index.html", "_self");
+  } else {
+    localStorage.clear();
+    window.open(PATH + "auth/login.html", "_self");
+  }
 }).fail(async function (data) {
   var obj = JSON.parse(data['responseText']);
   console.log(obj);
